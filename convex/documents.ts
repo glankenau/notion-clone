f1952fe08ac7d13579f7bc3f4f4a345d12953cdc
Gerardo M. Lankenau <72425967+glankenau@users.noreply.github.com>
2024-10-16 10:25:12 +0200

    Feature | Document List (#7)
    * Add new components to add recursive documents
    
    * Modify existing components to add files recursively

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,15 +3,15 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
-export const create = mutation ({
+export const create = mutation({
   args: {
     title: v.string(),
-    parentDocument: v.optional(v.id("documents"))
+    parentDocument: v.optional(v.id("documents")),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
-    if(!identity) {
-      throw new Error ("Not authenticated");
+    if (!identity) {
+      throw new Error("Not authenticated");
     }
 
     const userId = identity.subject;
@@ -21,21 +21,34 @@ export const create = mutation ({
       parentDocument: args.parentDocument,
       userId,
       isArchived: false,
-      isPublished: false
-    })
+      isPublished: false,
+    });
 
     return document;
-  }
+  },
 });
 
-export const get = query({
-  handler: async (ctx) => {
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
-    if(!identity) {
-      throw new Error ("Not authenticated");
+
+    if (!identity) {
+      throw new Error("Not Authenticated");
     }
 
-    const documents = await ctx.db.query("documents").collect();
+    const userId = identity.subject;
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user_parent", (q) =>
+        q.eq("userId", userId).eq("parentDocument", args.parentDocument),
+      )
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
+
     return documents;
-  }
+  },
 });
